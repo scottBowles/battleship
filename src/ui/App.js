27450a32f6b325a1f8955bb7d5ddf4d5f281ed0c
scottBowles <shbowles@gmail.com>
2020-08-22 16:17:13 -0400

    Show boats on player board but not on opponent board

--- src/ui/App.js
+++ src/ui/App.js
@@ -1,25 +1,23 @@
 import React from 'react'
 
 import Board from './Board'
-// import CpuBoard from './CpuBoard'
 // import PlayerDisplay from './PlayerDisplay'
 
 import Player from "../models/Player"
 import CPU from "../models/CPU"
 
-const player = Player("Player")
-const cpu = CPU()
 
 function App() {
+    const player = Player("Player")
+    const cpu = CPU()
     return (
         <div style={{ textAlign: "center" }}>
             <h1>BATTLESHIP!</h1>
             <div style={{ display: "flex", justifyContent: "space-around" }}>
-                <Board type="Own" gameboard={player.gameboard.board}/>
+                <Board type="Player" gameboard={player.gameboard.board}/>
                 <Board type="Opponent" gameboard={cpu.gameboard.board} />
             </div>
             {
-            // <CpuBoard />
             // <PlayerDisplay />
             }
         </div>
--- src/ui/Board.js
+++ src/ui/Board.js
@@ -2,19 +2,19 @@ import React from 'react'
 
 function Board(props) {
     const { type, gameboard } = props
-    const cells = gameboard.map((cell, index) => <div key={ index } style={ getCellStyle(cell) }></div>)
+    const cells = gameboard.map((cell, index) => <div key={ index } style={ getCellStyle(cell, type) }></div>)
     
     return (
         <div>
             <h3>{type} Board</h3>
-            <div style={ grid }>
+            <div style={ gridStyle }>
                 { cells }
             </div>
         </div>
     )
 }
 
-const grid = { 
+const gridStyle = { 
     height: "400px",
     width: "400px",
     display: "grid", 
@@ -23,36 +23,51 @@ const grid = {
     margin: "32px"
 }
 
-const getCellStyle = (cell) => {
-    const getColor = (cellValue) => {
-        switch (cellValue) {
-            case "hit":
-                return "red"
-                break
-            case "miss":
-                return "grey"
-                break
-            case "Carrier":
-                return "yellow"
-                break
-            case "Battleship":
-                return "green"
-                break
-            case "Cruiser":
-                return "orange"
-                break
-            case "Submarine":
-                return "blue"
-                break
-            case "Destroyer":
-                return "purple"
-                break
-            default:
-                return "white"
-                break
+const getCellStyle = (cell, boardType) => {
+    const getColor = (cellValue, boardType) => {
+        if (boardType === "Player") {
+            switch (cellValue) {
+                case "Carrier":
+                    return "yellow"
+                    break
+                case "Battleship":
+                    return "green"
+                    break
+                case "Cruiser":
+                    return "orange"
+                    break
+                case "Submarine":
+                    return "blue"
+                    break
+                case "Destroyer":
+                    return "purple"
+                    break
+                case "hit":
+                    return "red"
+                    break
+                case "miss":
+                    return "grey"
+                    break
+                default:
+                    return "white"
+                    break
+            }
+        }
+        if (boardType === "Opponent") {
+            switch(cellValue) {
+                case "hit":
+                    return "red"
+                    break
+                case "miss":
+                    return "grey"
+                    break
+                default:
+                    return "white"
+                    break
+            }
         }
     }
-    const color = getColor(cell)
+    const color = getColor(cell, boardType)
     return {
         border: "1px black solid",
         backgroundColor: color
