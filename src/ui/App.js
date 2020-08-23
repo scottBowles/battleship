53ff693393c19638e1c0f8576091346cd44c0cc8
scottBowles <shbowles@gmail.com>
2020-08-23 15:44:55 -0400

    Implement non-turn phases with buttons and display for each

--- src/ui/App.js
+++ src/ui/App.js
@@ -3,34 +3,49 @@ import React, { useState } from 'react'
 import Board from './Board'
 import PlayerDisplay from './PlayerDisplay'
 
-function App(props) {
-    const { player, opponent } = props
+import Player from "../models/Player"
+import cpuFactory from "../models/CPU"
 
+function App() {
+    const [player, setPlayer] = useState(Player("Player"))
+    const [opponent, setOpponent] = useState(cpuFactory())
     const [playerBoard, setPlayerBoard] = useState(player.gameboard.board)
     const [opponentBoard, setOpponentBoard] = useState(opponent.gameboard.board)
-    const [phase, setPhase] = useState("playerTurn")
+    const [phase, setPhase] = useState("setup")
+    const [winningPlayer, setWinningPlayer] = useState()
 
     const playerAttack = (position) => {
         const playerAttackResult = opponent.receiveAttack(position)
         if (playerAttackResult === "Position already attacked") return false
         setOpponentBoard([...playerAttackResult])
-        if (opponent.allSunk()) endGame(player)
-        setPhase("opponentTurn")
-        return true
+        if (opponent.allSunk()) {
+            endGame(player)
+        } else {
+            setPhase("opponentTurn")
+            return true
+        }
     }
 
     const opponentAttack = (position) => {
         const attackedPosition = opponent.attack()
         const opponentAttackResult = player.receiveAttack(attackedPosition)
         setPlayerBoard([...opponentAttackResult])
-        if (player.allSunk()) endGame(opponent)
-        setPhase("playerTurn")
+        if (player.allSunk()) {
+            endGame(opponent)
+        } else {
+            setPhase("playerTurn")
+        }
+    }
+
+    const endGame = (winningPlayer) => {
+        setPhase("endgame")
+        setWinningPlayer(winningPlayer)
     }
 
     const handleCellClick = (position, whoseBoard) => {
         if (whoseBoard !== "Opponent" || phase !== "playerTurn") return
         const isValidPlay = playerAttack(position)   
-        if (isValidPlay) {
+        if (isValidPlay && phase !== "endgame") {
             setTimeout(() => {
                 opponentAttack(position)
             }, 300)
@@ -39,8 +54,24 @@ function App(props) {
 
     const handlePlaceShipsRandomly = () => {
         const { ships, board } = player.gameboard
+        console.log({ships, board})
         const updatedBoard = player.placeShipsRandomly(ships, board)
         setPlayerBoard([...updatedBoard])
+    }
+
+    const startGame = () => {
+        setPhase("playerTurn")
+    }
+
+    const newGame = () => {
+        setPhase("setup")
+        const playerName = player.name
+        const newPlayer = Player(playerName || "Player")
+        const newOpponent = cpuFactory()
+        setPlayer(newPlayer)
+        setPlayerBoard([...newPlayer.gameboard.board])
+        setOpponent(newOpponent)
+        setOpponentBoard([...newOpponent.gameboard.board])
     }
 
     return (
@@ -50,7 +81,7 @@ function App(props) {
                 <Board whoseBoard="Player" name={ player.name } gameboard={ playerBoard } handleCellClick={ handleCellClick } />
                 <Board whoseBoard="Opponent" name={ opponent.name } gameboard={ opponentBoard } handleCellClick={ handleCellClick } />
             </div>
-            <PlayerDisplay handlePlaceShipsRandomly={ handlePlaceShipsRandomly } />
+            <PlayerDisplay phase={ phase } winningPlayer={ winningPlayer } handlePlaceShipsRandomly={ handlePlaceShipsRandomly } startGame={ startGame } newGame={ newGame } player={ player } opponent={ opponent }/>
         </div>
     )
 }
--- src/ui/PlayerDisplay.js
+++ src/ui/PlayerDisplay.js
@@ -1,11 +1,36 @@
 import React from 'react'
 
 function PlayerDisplay(props) {
-    return (
-        <div>
-            <button name="placeShipsRandomly" onClick={ props.handlePlaceShipsRandomly }>Place Ships Randomly</button>
-        </div>
-    )
+    const { phase, handlePlaceShipsRandomly, startGame, newGame, winningPlayer, player, opponent } = props
+
+    return phase === "setup"
+        ? (
+            <div>
+                <h3>Place Your Ships!</h3>
+                <button name="placeShipsRandomly" onClick={ handlePlaceShipsRandomly }>Place Ships Randomly</button>
+                <button name="startGame" onClick={ startGame }>Start Game</button>
+            </div>
+        )
+        : phase === "endgame" 
+        ? (
+            <div>
+                <h3>{ winningPlayer.name } Wins!</h3>
+                <button onClick={ newGame }>New Game?</button>
+            </div>
+        )
+        : phase === "playerTurn"
+        ? (
+            <div>
+                <h3>{player.name}'s Turn</h3>
+            </div>
+        )
+        : phase === "opponentTurn"
+        ? (
+            <div>
+                <h3>{opponent.name}'s Turn</h3>
+            </div>
+        )
+        : null
 }
 
 export default PlayerDisplay
