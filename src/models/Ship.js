0fcc495414984d32138781e74d9eff7850f8d424
scottBowles <shbowles@gmail.com>
2020-08-24 21:29:43 -0400

    Add direction property to ships

--- src/models/Ship.js
+++ src/models/Ship.js
@@ -1,17 +1,22 @@
-function shipFactory(length, name, color) {
-    let unHitPips = length
-    const hit = () => {
-        unHitPips -= 1
-        return isSunk() ? "sunk" : false
-    }
-    const isSunk = () => unHitPips < 1
-    return {
+function shipFactory(length, name, color, direction="horizontal") {
+    const ship = {
         length,
         name,
         color,
-        hit,
-        isSunk
+        direction,
+        unHitPips: length,
     }
+
+    ship.changeDirection = (newDirection) => ship.direction = newDirection
+    
+    ship.hit = () => {
+        ship.unHitPips -= 1
+        return ship.isSunk() ? "sunk" : false
+    }
+
+    ship.isSunk = () => ship.unHitPips < 1
+
+    return ship
 }
 
 export default shipFactory
--- src/ui/DraggableShips.js
+++ src/ui/DraggableShips.js
@@ -5,10 +5,27 @@ function DraggableShips(props) {
     return(
         <div style={{ display: "flex" }}>
             { ships.map(ship => (
-                <div key={ ship.name } onDragStart={ props.dragStart } draggable data-name={ship.name} style={{height: "30px", width: `${ship.length * 40}px`, margin: "5px", backgroundColor: "pink"}}></div>    
-            )) }
+                <div 
+                    key={ ship.name } 
+                    ship={ ship } 
+                    onDragStart={ props.dragStart } 
+                    draggable data-name={ship.name} 
+                    style={ shipStyles(ship) }
+                >
+                </div>    
+            )) 
+            }
         </div>
     )
+}
+
+function shipStyles(ship) {
+    return {
+            height: "30px", 
+            width: `${ship.length * 40}px`, 
+            margin: "5px", 
+            backgroundColor: ship.color
+    }
 }
 
 export default DraggableShips
