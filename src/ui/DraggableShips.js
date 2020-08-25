72bc3ced2d674d9780a652c72fdf61015da0216d
scottBowles <shbowles@gmail.com>
2020-08-24 22:01:49 -0400

    Create room in DraggableShips component for ships to turn vertical

--- src/ui/DraggableShips.js
+++ src/ui/DraggableShips.js
@@ -3,25 +3,29 @@ import React from 'react'
 function DraggableShips(props) {
     const { ships, dragStart } = props
     return(
-        <div style={{ display: "flex" }}>
-            { ships.map(ship => (
-                <div 
-                    key={ ship.name } 
-                    ship={ ship } 
-                    onDragStart={ props.dragStart } 
-                    draggable data-name={ship.name} 
-                    style={ shipStyles(ship) }
-                >
-                </div>    
-            )) 
-            }
+        <div>
+            <div style={{ display: "flex", marginLeft: "20px" }}>Drag in place. Right click ship below to change direction.</div>
+            <div style={{ display: "flex" }}>
+                { ships.map(ship => (
+                    <div key={ ship.name } style={{ height: `${ship.length * 40}px`, width: `${ship.length * 40}px`, margin: "10px" }}>
+                        <div
+                            ship={ ship } 
+                            onDragStart={ props.dragStart } 
+                            draggable 
+                            data-name={ship.name} 
+                            style={ shipStyles(ship) }
+                        >
+                        </div>
+                    </div>
+                )) }
+            </div>
         </div>
     )
 }
 
 function shipStyles(ship) {
     return {
-            height: "30px", 
+            height: "40px", 
             width: `${ship.length * 40}px`, 
             margin: "5px", 
             backgroundColor: ship.color
